d2fb9ffc8dd7c0b7783ed7a81c695456d9af8e35
SINTAXY\belarmino <belarmino.veiga@gmail.com>
2022-08-27 15:33:26 -0100

    Web Socket

---
+++ Hubs/Shub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebSocket.Hubs
+{
+    public class Shub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
